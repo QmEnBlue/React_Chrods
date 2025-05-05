b0e513e2c702f3bc6a1836a20c737461c9248ee7
QmEn_BLue <qmen.blue@gmail.com>
2025-05-06 01:20:24 +0300

    I have created a component Layout and and also simple routing.

---
+++ src/Router/Layout.tsx
@@ -0,0 +1,45 @@
+import { Link, Outlet } from "react-router-dom"
+
+import { GlobalSvgSelector } from "../assets/GlobalSvgSelector"
+import s from './Layout.module.scss'
+
+export const Layout = () => {
+    return (
+        <>
+            <header>
+                <Link to="/">
+                    <span className={s.heding_link}>
+                        <span className={s.icon_chord}>
+                            <GlobalSvgSelector id="chord-logo" />
+                        </span>
+                        <span className={s.heding}>
+                            <span className={s.heding_up}>
+                                REACT CHORDS
+                            </span>
+                            <span className={s.heding_down}>
+                                Музыка вкуса и палитра звучания
+                            </span>
+                        </span>
+                    </span>
+                </Link>
+                <span className={s.input}>
+                    <span className={s.icon_loupe}>
+                        <GlobalSvgSelector id="loupe"/>
+                    </span>
+                    <input type="text" placeholder="Поиск песни..." className={s.input_value}/>
+                </span>
+                <button className={s.auth_button}>
+                    <span className={s.icon_persone}>
+                        <GlobalSvgSelector id="persone"/>
+                    </span>
+                    <span className={s.auth_name}>
+                        Войти
+                    </span>
+                </button>
+            </header>
+            <div className={s.Outlet}>
+                <Outlet/>
+            </div>
+        </>
+    )
+}
---
+++ src/Router/Router.tsx
@@ -0,0 +1,17 @@
+import { Routes, Route } from "react-router-dom"
+import { Main } from "../pages/main/main"
+import { Layout } from "./Layout"
+import { AuthPage } from "../pages/auth/auth"
+import { Home } from "../pages/home/home"
+
+export const RouterPages = () => {
+    return(
+        <Routes>
+            <Route path="/" element={<Layout/>}>
+                <Route index element={<Main/>}/>
+                <Route path="Home" element={<Home/>}/>
+                <Route path="Auth" element={<AuthPage/>}/>
+            </Route>
+        </Routes>
+    )
+}
